fc05125a0c87b876d0f2a337c53ae376f6db340c
Ahady <ahadyvamuleke@gmail.com>
2021-02-24 05:32:15 +0200

    Fix Bootstrap rounded image in ProfileHome component.

--- frontend/src/components/profile/ProfileHome.js
+++ frontend/src/components/profile/ProfileHome.js
@@ -6,15 +6,14 @@ const ProfileHome = () => {
         <div className="main">
             <div className="main-content">
 
-                <div className="col-md-3 bg-dark"></div>
+                <div className="col-md-3"></div>
             
-                <div className="text-center col-md-6" style={{marginTop : "30px"}}>
-                    <img src={process.env.PUBLIC_URL + "/assets/images/user-medium.png"} className="rounded" style={{border : "2px solid white", borderRadius : "500px"}} width="150" height="150"/>
+                <div className="text-center col-md-6 mt-5">
+                    <img src={process.env.PUBLIC_URL + "/assets/images/belle.jpg"} className="rounded-circle" width="200" height="200"/>
                     <div className="h3">Donatien Vamuleke</div>
                     <div className="h4">donatien@yahoo.fr</div>
                     <div className="h4">Administrateur</div>
 
-                    {/* <button className="btn btn-primary btn-block">Modifier mon profil</button> */}
                     <Link to='/admin/profile/create' className="btn btn-primary btn-block">Modifier mon profil</Link>
                 </div>
 
