2d249484c5a338433feef11177272aa11b4f6ece
Ahady <ahadyvamuleke@gmail.com>
2021-02-27 16:43:33 +0200

    Fix type in index.js file.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux';
 import store from './store.js'
-// import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 ReactDOM.render(
     <Provider store={store}>
